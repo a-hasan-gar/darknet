e9d629634d9f68b635d12cd4b69407765fc3d6c4
Ahmad Hasan Siregar <a.hasan.gar@gmail.com>
2019-05-20 22:30:14 +0700

    Adjust video_yolov3.sh to current environment

--- video_yolov3.sh
+++ video_yolov3.sh
@@ -1,6 +1,6 @@
 
 
-./darknet detector demo ./cfg/coco.data ./cfg/yolov3.cfg ./yolov3.weights test50.mp4 -i 0 -thresh 0.25
+./darknet detector demo cfg/coco.data cfg/yolov3.cfg yolov3.weights tests/video.mp4 -out_filename out.avi -dont_show -gpus 0,1,2 -thresh 0.8
 
 
 
